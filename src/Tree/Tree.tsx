4b9d12bea53015241f6f5ecf877ce0055864fbc5
anannanya <2416672966@qq.com>
2022-03-31 22:47:30 +0800

    等待debug及优化

--- src/Tree/Tree.tsx
+++ src/Tree/Tree.tsx
@@ -4,7 +4,7 @@ import React, { useCallback } from "react";
 import List, { IBaseListItem } from "../List";
 import { IListProps, ListController } from "../List/List";
 import { useFlatten } from "./hooks/useFlatten";
-import "./index.less";
+import "./index.css";
 import 'antd/dist/antd.css'
 
 export interface IBaseTreeNodeData<ITreeNodeData> extends IBaseListItem {
